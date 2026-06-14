303a2d02ec427f399d30bad3667182926c7085a1
Erkyhanma <yenzel.bt@gmail.com>
2026-06-14 12:58:08 -0400

    feat: add Footer to Book feed pages

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -2,13 +2,14 @@ import { Link } from "react-router";
 
 type LogoProps = {
   route: string;
+  color?: string;
 };
 
-const Logo = ({ route }: LogoProps) => {
+const Logo = ({ route, color = "text-foreground" }: LogoProps) => {
   return (
     <Link to={route} className="flex items-center gap-1">
       <img className="h-auto w-8" src="/images/Logo.png" alt="Logo" />
-      <p className="text-2xl font-semibold">LibraryMs</p>
+      <p className={`text-2xl font-semibold ${color}`}>LibraryMs</p>
     </Link>
   );
 };
